bf43738fb1fe2c0dadd43df4f41095d2ba07cb7b
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-11-17 13:28:45 +0200

    Setup

---
+++ server.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import { serve } from 'inngest/express';
+import connectDB from './configs/db.js';
+import { functions, inngest } from './inngest/index.js';
+
+const app = express();
+await connectDB();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Server Is Running');
+});
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use('/api/inngest', serve({ client: inngest, functions }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server Is Running on ${PORT}`));
