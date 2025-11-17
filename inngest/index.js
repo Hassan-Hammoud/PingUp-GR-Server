4bf670f7e3a525d3af1360f0522337d91f70af66
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-11-17 13:44:42 +0200

    Implement Inngest functions for user creation, update, and deletion

--- inngest/index.js
+++ inngest/index.js
@@ -1,7 +1,68 @@
 import { Inngest } from 'inngest';
+import User from './../models/User.js';
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: 'pingup-app' });
 
+// INNGEST FUNCTION TO SAVE USER DATA TO A DATABASE
+
+const syncUserCreation = inngest.createFunction(
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
+  async event => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split('@')[0];
+
+    // CHECK AVAILABILITY OF USERNAME
+    const user = await User.findOne({ username });
+
+    if (user) {
+      username = username + Math.floor(Math.random() * 10000);
+    }
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      full_name: first_name + ' ' + last_name,
+      profile_picture: image_url,
+      username,
+    };
+
+    await User.create(userData);
+  }
+);
+
+// INNGEST FUNCTION TO UPDATE USER DATA TO A DATABASE
+
+const syncUserUpdated = inngest.createFunction(
+  { id: 'update-user-from-clerk' },
+  { event: 'clerk/user.updated' },
+  async event => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const updatedUserData = {
+      email: email_addresses[0].email_address,
+      full_name: first_name + ' ' + last_name,
+      profile_picture: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, updatedUserData);
+  }
+);
+
+// INNGEST FUNCTION TO DELETE USER DATA FROM DATABASE
+
+const syncUserDeleted = inngest.createFunction(
+  { id: 'delete-user-with-clerk' },
+  { event: 'clerk/user.deleted' },
+  async event => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUserCreation, syncUserUpdated, syncUserDeleted];
